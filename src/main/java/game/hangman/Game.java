1c630ad21edcefe83bbb7b54dfe61907c02c8f2d
Gar-Yin Lam <garyin.lam@gmail.com>
2022-09-28 13:01:26 +0100

    Refactor main out to Game class

--- src/main/java/game/hangman/Game.java
+++ src/main/java/game/hangman/Game.java
@@ -1,5 +1,10 @@
 package game.hangman;
 
+import java.util.Scanner;
+
+import static game.hangman.Input.getGuess;
+import static game.hangman.Output.displayTurn;
+import static game.hangman.Output.drawStick;
 import static game.hangman.WordUtils.*;
 
 public class Game {
@@ -21,5 +26,45 @@ public class Game {
 
     public Word getWordObject() {
         return word;
+    }
+
+    public void doGame(Scanner scanner){
+        boolean gameOver = false;
+
+        // game start
+        while (!gameOver) {
+            displayTurn(player,word);
+            drawStick(player.getLives());
+            char guess = getGuess(scanner);
+
+            if (player.isGuessValid(guess)) {
+                boolean correct = word.makeGuess(guess);
+                player.addGuess(guess);
+
+                if (correct) {
+                    System.out.println(guess + " was correct!");
+                } else {
+                    player.removeLife();
+                }
+            } else {
+                System.out.println("Already guessed " + guess + ". Try again.");
+            }
+
+            System.out.println("\b \n");
+            gameOver = isGameOver();
+        }
+
+        gameOver();
+    }
+
+    private void gameOver(){
+        // game over
+        if (player.isLost()) {
+            drawStick(-1);
+            System.out.println("You lost!");
+        } else {
+            System.out.println("You win!\nWith " + player.getLives() + " guesses remaining!");
+        }
+        System.out.println("The word was: " + word.getWord());
     }
 }
--- src/main/java/game/hangman/Main.java
+++ src/main/java/game/hangman/Main.java
@@ -4,7 +4,6 @@ package game.hangman;
 import java.util.Scanner;
 
 import static game.hangman.Input.*;
-import static game.hangman.Output.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,47 +13,9 @@ public class Main {
 
         while (!isFinished) {
             // game setup
-            System.out.println("How many lives would you like? (max 10)");
-            int lives = getInt(scanner,1,10);
-            System.out.println("What category of words would you like?");
-            System.out.println("1 - Normal words");
-            System.out.println("2 - Movies");
-            System.out.println("3 - K-pop groups");
-            int category = getInt(scanner,1,3);
-            Game game = new Game(lives,category);
-            boolean gameOver = false;
-
-            // game start
-            while (!gameOver) {
-                displayTurn(game.getPlayer(),game.getWordObject());
-                drawStick(game.getPlayer().getLives());
-                char guess = getGuess(scanner);
-
-                if (game.getPlayer().isGuessValid(guess)) {
-                    boolean correct = game.getWordObject().makeGuess(guess);
-                    game.getPlayer().addGuess(guess);
-
-                    if (correct) {
-                        System.out.println(guess + " was correct!");
-                    } else {
-                        game.getPlayer().removeLife();
-                    }
-                } else {
-                    System.out.println("Already guessed " + guess + ". Try again.");
-                }
-
-                System.out.println("\b \n");
-                gameOver = game.isGameOver();
-            }
-
-            // game over
-            if (game.getPlayer().isLost()) {
-                drawStick(-1);
-                System.out.println("You lost!");
-            } else {
-                System.out.println("You win!\nWith " + game.getPlayer().getLives() + " guesses remaining!");
-            }
-            System.out.println("The word was: " + game.getWordObject().getWord());
+            int[] gameParams = setUp(scanner);
+            Game game = new Game(gameParams[0],gameParams[1]);
+            game.doGame(scanner);
 
             // play again
             System.out.println("\nPlay again? (y/n)");
@@ -62,5 +23,17 @@ public class Main {
         }
 
         scanner.close();
+    }
+
+    public static int[] setUp(Scanner scanner){
+        int[] retVal = new int[2];
+        System.out.println("How many lives would you like? (max 10)");
+        retVal[0] = getInt(scanner,1,10);
+        System.out.println("What category of words would you like?");
+        System.out.println("1 - Normal words");
+        System.out.println("2 - Movies");
+        System.out.println("3 - K-pop groups");
+        retVal[1] = getInt(scanner,1,3);
+        return retVal;
     }
 }
